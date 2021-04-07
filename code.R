f641ddbad03fdc389d7532bbc24193b7dcdd55d9
inigofernandez <62943191+inigofernandez@users.noreply.github.com>
2021-04-07 21:06:50 +0200

    Add files via upload

---
+++ code.R
@@ -0,0 +1,55 @@
+unzip("./repdata_data_activity.zip")
+actData <- read.csv("./activity.csv")
+summary(actData)
+names(actData)
+
+stepsPerDay <- aggregate(steps ~ date, actData, sum, na.rm=TRUE)
+hist(stepsPerDay$steps)
+meanStepsPerDay <- mean(stepsPerDay$steps)
+meanStepsPerDay
+
+medianStepsPerDay <- median(stepsPerDay$steps)
+medianStepsPerDay
+
+stepsPerInterval<-aggregate(steps~interval, data=actData, mean, na.rm=TRUE)
+plot(steps~interval, data=stepsPerInterval, type="l")
+
+intervalWithMaxNbSteps <- stepsPerInterval[which.max(stepsPerInterval$steps),]$interval
+intervalWithMaxNbSteps
+
+totalValuesMissings <- sum(is.na(actData$steps))
+totalValuesMissings
+
+getMeanStepsPerInterval<-function(interval){
+  stepsPerInterval[stepsPerInterval$interval==interval,]$steps
+}
+
+activityDataNoNA<-actData
+for(i in 1:nrow(activityDataNoNA)){
+  if(is.na(activityDataNoNA[i,]$steps)){
+    activityDataNoNA[i,]$steps <- getMeanStepsPerInterval(activityDataNoNA[i,]$interval)
+  }
+}
+
+totalStepsPerDayNoNA <- aggregate(steps ~ date, data=activityDataNoNA, sum)
+hist(totalStepsPerDayNoNA$steps)
+
+meanStepsPerDayNoNA <- mean(totalStepsPerDayNoNA$steps)
+medianStepsPerDayNoNA <- median(totalStepsPerDayNoNA$steps)
+
+activityDataNoNA$date <- as.Date(strptime(activityDataNoNA$date, format="%Y-%m-%d"))
+activityDataNoNA$day <- weekdays(activityDataNoNA$date)
+for (i in 1:nrow(activityDataNoNA)) {
+  if (activityDataNoNA[i,]$day %in% c("Saturday","Sunday")) {
+    activityDataNoNA[i,]$day<-"weekend"
+  }
+  else{
+    activityDataNoNA[i,]$day<-"weekday"
+  }
+}
+stepsByDay <- aggregate(activityDataNoNA$steps ~ activityDataNoNA$interval + activityDataNoNA$day, activityDataNoNA, mean)
+
+names(stepsByDay) <- c("interval", "day", "steps")
+library(lattice)
+xyplot(steps ~ interval | day, stepsByDay, type = "l", layout = c(1, 2), 
+       xlab = "Interval", ylab = "Number of steps")
